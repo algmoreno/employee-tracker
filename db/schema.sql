556c174025f7bf5600e847ddb7201642b40cdc5b
algmoreno <alg.moreno00@gmail.com>
2021-07-24 13:26:03 -0700

    all queries working except update

--- db/schema.sql
+++ db/schema.sql
@@ -5,23 +5,23 @@ USE employee_tracker;
 
 CREATE TABLE department(
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INTEGER,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) 
 );
 
 CREATE TABLE employee(
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INTEGER,
-  FOREIGN KEY (role_id) REFERENCES role(id),
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
   manager_id INTEGER,
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const connection = require('./config/connection');
 const inquirer = require('inquirer');
+const { connect } = require('./config/connection');
 
 function init() {
   startQuestions();
@@ -23,20 +24,30 @@ function startQuestions() {
   )
     .then(results => {
       if (results.option === 'View all Departments') {
-        connection.query('SELECT * FROM department', results, (err, results) => {
-          if (err) {
-            console.log(err)
-          }
-          console.log(results)
-        })
-      }
+          connection.query('SELECT * FROM department', (err, deptTable) => {
+            console.table(deptTable)
+            if (deptTable.length < 1) {
+              console.log("Table is empty!")
+            }
+      })
+    }
 
       else if (results.option === 'View all Roles') {
-
+          connection.query('SELECT * FROM role', (err, roleTable) => {
+            console.table(roleTable)
+            if (roleTable.length < 1) {
+              console.log("Table is empty!")
+            }
+          })
       }
 
       else if (results.option === 'View all Employees') {
-
+        connection.query('SELECT * FROM employee', (err, employeeTable) => {
+          console.table(employeeTable)
+          if (employeeTable.length < 1) {
+            console.log("Table is empty!")
+          }
+        })
       }
 
       else if (results.option === 'Add a Department') {
@@ -77,7 +88,7 @@ function startQuestions() {
               {
                 type: 'text',
                 name: 'department_id',
-                message: 'Enter the department id from table above'
+                message: 'Enter the department id (see table above)'
               }])
               .then(role => {
                 console.log(role)
@@ -107,41 +118,79 @@ function startQuestions() {
       }
 
       else if (results.option === 'Add an Employee') {
-        inquirer.prompt(
-          {
-            type: 'text',
-            name: 'first_name',
-            message: "Enter employee's first name"
-          },
-          {
-            type: 'text',
-            name: 'last_name',
-            message: "Enter employee's last name"
-          },
-          {
-            type: 'text',
-            name: 'role_id',
-            message: "What is this employee's role?"
-          },
-          {
-            type: 'text',
-            name: 'manager_id',
-            message: "Enter the employee's manager?"
-          }
-        )
-          .then(results => {
-            console.log(results)
-
-            connection.query('INSERT INTO role SET ? ', results, (err, results) => {
-              if (err) {
-                console.log('error')
+        connection.query('SELECT * FROM role', (err, roleTable) => {
+          if (roleTable.length > 0) {
+            console.table(roleTable)
+            inquirer.prompt([
+              {
+                type: 'text',
+                name: 'first_name',
+                message: "Enter employee's first name"
+              },
+              {
+                type: 'text',
+                name: 'last_name',
+                message: "Enter employee's last name"
+              },
+              {
+                type: 'text',
+                name: 'role_id',
+                message: "Enter id that matches the employee's role (see table above)"
               }
-              console.log('Employee Added')
-            })
-          })
+              // {
+              //   type: 'text',
+              //   name: 'manager_id',
+              //   message: "Enter the employee manager's id (see table above)"
+              // }
+            ])
+              .then(employee => {
+                console.log(employee)
+
+                connection.query('INSERT INTO employee SET ? ', employee, (err, row) => {
+                  if (err) {
+                    console.log(err)
+                    return
+                  }
+                  else {
+                    console.log('Employee Added')
+                  }
+                })
+              })
+          }
+          else {
+            console.log(`
+            ----------------------------------------
+            You need to add a role first!
+            ----------------------------------------`
+            );
+            startQuestions();
+          }
+
+        })
       }
 
       else if (results.option === 'Update an Employee Role') {
+        connection.query('SELECT * FROM role', (err, roleTable) => {
+          console.table(roleTable)
+        })
+        inquirer.prompt([
+          {
+            type: 'text',
+            name: 'role_id',
+            message: "Enter the employee's new role id"
+          }
+        ])
+        .then(newRole => {
+          connection.query('UPDATE employee SET role_id = ? WHERE id = ?', newRole, (err, results) => {
+            if (err) {
+              console.log(err)
+              return
+            }
+            else {
+              console.log('Employee role updated')
+            }
+          })
+        })
 
       }
     })
